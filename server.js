0d790c29c8ba85dd7fdc31d2c5d6d7afffe68bcf
DzaninMasic <dzanin.masic@stu.ibu.edu.ba>
2022-09-14 09:09:18 +0200

    Authentication prototype added

--- server.js
+++ server.js
@@ -5,11 +5,48 @@ import cors from 'cors'
 import categoryRouter from './routers/category.router'
 import subcategoryRouter from './routers/subcategory.router'
 import itemRouter from './routers/item.router';
+import jwt from 'jsonwebtoken'
 
 const server=express();
 const PORT=8000;
 
 const init=() => {
+
+    const posts = [
+        {
+          username: 'Dzanin',
+          title: 'Test'
+        },
+        {
+          username: 'Random User',
+          title: 'Test 2'
+        }
+    ]
+
+    server.get('/posts', authenticateToken, (req, res) => {
+        res.json(posts.filter(post => post.username === req.user.name))
+      })
+
+    server.post('/login', (req, res) => {
+        const username = req.body.username
+        const user = {name: username}
+
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+        res.json({ accessToken : accessToken })
+    })
+      
+    function authenticateToken(req, res, next) {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (token == null) return res.sendStatus(401)
+      
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+          console.log(err)
+          if (err) return res.sendStatus(403)
+          req.user = user
+          next()
+        })
+    }
 
     server.use(express.json());
     server.use(cors());
